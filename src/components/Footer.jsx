595ea992f43eca8907d7c1d6958367164476b4dc
yashwanth <yashwanthreddy7675@gmail.com>
2024-02-22 01:28:50 +0530

    projects updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { FaRegStar } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div id='footer' className='w-full h-auto bg-custom'>
-         <h1 className='py-2 pt-10 text-xs text-center text-white '> Designed and built by Yashwanth</h1>
+    <div id='footer' className='flex justify-center w-full h-auto gap-2 py-10 bg-custom'>
+         <h1 className='text-xs text-center text-white '> Designed and built by Yashwanth</h1>
+         <span>
+          <Link onClick={() => window.open("https://github.com/Yashwanthreddy711/Portfolio", "_blank")} className='text-white'><FaRegStar/></Link>
+           </span>
     </div>
 
   )
