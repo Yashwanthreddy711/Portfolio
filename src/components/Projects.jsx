1ad34f2ca203ca452927b092565562294747ecf9
yashwanth <yashwanthreddy7675@gmail.com>
2024-02-21 23:03:48 +0530

    scroll bar updated

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -13,7 +13,7 @@ const Projects = () => {
               PROJECTS
                      </p>
             </div>
-            <div className='flex flex-col pl-0 mt-20 overflow-y-hidden md:flex-row'>
+            <div className='flex flex-col pl-0 mx-auto mt-20 overflow-y-hidden md:flex-row'>
               <div className='pb-10 ml-10 mr-10'>
                 <img className='w-96 overflow-y-hidden md:w-[800px] cursor-pointer first-letter:  border-2 rounded-lg h-60'src={Project1} alt="" />
               </div>
@@ -30,7 +30,7 @@ const Projects = () => {
                  </div>
               </div>
             </div>
-            <div className='flex flex-col pt-10 pl-0 mt-10 md:flex-row'>
+            <div className='flex flex-col pt-10 pl-0 mx-auto mt-10 md:flex-row'>
               <div className='pl-10 max-w-[500px] overflow-y-hidden  ' >
                  <p className='text-[#50C9CD] font-bold text-2xl flex pl-5 md:text-4xl overflow-y-hidden '>CRYPTO BOLT</p>
                  <p className='pt-5 pl-5 overflow-y-hidden text-xl text-gray-400' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed quibusdam porro praesentium ipsa libero, ex dolores fuga obcaecati. Exercitationem, corporis?</p>
@@ -54,7 +54,7 @@ const Projects = () => {
           to="/moreprojects"
           >
           
-          <button className='relative flex items-center justify-center p-2 px-6 py-2 overflow-hidden font-medium transition duration-300 ease-out border-2 rounded-full md:p-4 md:px-12 md:py-4 text-custom2 border-custom2 group'>
+          <button className='relative flex items-center justify-center p-2 px-6 py-2 mx-auto mt-16 overflow-hidden font-medium transition duration-300 ease-out border-2 rounded-full md:p-4 md:px-12 md:py-4 text-custom2 border-custom2 group'>
             <span className="absolute flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-custom group-hover:translate-x-0">
               <svg 
               className="w-5 h-5" 
@@ -71,8 +71,8 @@ const Projects = () => {
               </path>
               </svg>
             </span>
-            <span className="absolute flex items-center justify-center w-full h-full transition-all duration-300 transform text-custom2 group-hover:translate-x-full ease">View Work</span>
-            <span className="relative invisible sm:text-sm">View Work</span>
+            <span className="absolute flex items-center justify-center w-full h-full transition-all duration-300 transform text-custom2 group-hover:translate-x-full ease">Other Projects</span>
+            <span className="relative invisible sm:text-sm">Other Projects</span>
           </button>
           </Link>
           </div>
